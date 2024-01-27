0137442de3007d6e719864337f70fc272ea4b49c
Hugo Silva <hugo.marcelo@clevertech.biz>
2024-01-27 11:14:02 -0300

    feat: update header layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import './globals.css'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -15,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className={inter.variable} lang="pt">
-      <body>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
